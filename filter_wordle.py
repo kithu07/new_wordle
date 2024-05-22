d2e20c40a4512d0f862416ec4873238b97fc9dc3
Ridhy Arora <ridhyarora@gmail.com>
2024-05-22 20:21:00 +0530

    Adding code

--- filter_wordle.py
+++ filter_wordle.py
@@ -8,12 +8,12 @@ def match_exact(words: list, letter: str, pos:int):
 def match_excludes(words: list, letter: str, pos: int):
     return [word for word in words if letter not in word]
 
-filter = {'R': match_excludes, 'Y': match_excludes, 'G': match_exact}
+filter = {'R': match_excludes, 'Y': match_contains, 'G': match_exact}
 
-def choose(words: list, guess: str='', feedback: str=''):
+def matches(words: list, guess: str='', feedback: str=''):
     for pos, (letter, fb) in enumerate(zip(guess, feedback)):
         words = filter[fb](words, letter, pos)
-    return random.choice(words)
+    return (words)
 
 def exact_words(filename: str, size: int=5):
     with open(filename, 'r') as f:
