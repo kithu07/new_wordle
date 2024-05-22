ca1224f8e1921587b3d7b8c02544baf25e24c89d
Keerthana A R <keerthanaar0608@gmail.com>
2024-05-23 00:01:44 +0530

    Linked Python and json api

---
+++ requests-test.py
@@ -0,0 +1,28 @@
+import requests, json, random
+from filter_wordle import matches, sowpod_words
+
+s = requests.Session()
+
+register_data = {"name": "Keerthana"}
+
+headers = {'Content-Type' : 'application/json'}
+
+reg_resp = s.post('https://we6.talentsprint.com/game/register', data=json.dumps(register_data), headers=headers)
+print(reg_resp.json())
+id = reg_resp.json()['id']
+
+create_data={"id": id, "overwrite": True}
+
+create_resp= s.post('https://we6.talentsprint.com/game/create', data=json.dumps(create_data), headers=headers)
+
+print(create_resp.json())
+
+current_guess = ''
+current_feedback = ''
+
+guess_data = {"id": id, "guess": random.choice(matches(sowpod_words, current_guess, current_feedback))}
+
+while (current_feedback != 'GGGGG' and current_feedback != None):
+    api_feedback = s.post(...)
+    current_feedback = api_feedback['feedback']
+    next_guess = choose()
